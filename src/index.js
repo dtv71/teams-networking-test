03b77220f15bb04c75425a6b1d9fa7df65f71841
dan.tarta <dan.tarta@IndecoSoft.onmicrosoft.com>
2023-06-22 20:31:03 +0300

    display teams ftom json into table

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import "./style.css";
-// starting the app
+function $(selector) {
+  return document.querySelector(selector);
+}
+function getTeamsAsHtml(team) {
+  const row = `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>✖ &#9998</td>
+    </tr>`;
+  return row;
+}
+function displayTeams(teams) {
+  const teamsHTML = teams.map(getTeamsAsHtml);
+  const tbody = $("#teamsTable tbody");
+  tbody.innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then(r => r.json())
     .then(teams => {
-      console.warn(teams);
+      displayTeams(teams);
     });
 }
 

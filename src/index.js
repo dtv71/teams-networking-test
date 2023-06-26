941573792f25b9b5b3775e0980079d65d26eb012
dtv71 <developer@codecloud.ro>
2023-06-26 19:13:24 +0300

    delete team

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import "./style.css";
 function $(selector) {
   return document.querySelector(selector);
 }
+
+function deleteTeamRequest(id) {
+  return fetch("http://localhost:3000/teams-json/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ id: id })
+  }).then(r => r.json());
+}
+
 function getTeamsAsHtml(team) {
   const row = `<tr>
     <td>${team.promotion}</td>
     <td>${team.members}</td>
     <td>${team.name}</td>
     <td><a href="${team.url}" target="_blank">${team.url}</a></td>
-    <td>✖ &#9998</td>
+    <td>
+    <a data-id="${team.id}" class="remove-btn" href="#">✖</a> 
+    <a class="edit-btn" href="#">&#9998</a></td>
     </tr>`;
   return row;
 }
@@ -19,11 +32,31 @@ function displayTeams(teams) {
 }
 
 function loadTeams() {
-  fetch("teams.json")
+  fetch("http://localhost:3000/teams-json", {
+    method: "GET",
+    headers: {
+      "Content-Type": "appplication/json"
+    }
+  })
     .then(r => r.json())
     .then(teams => {
       displayTeams(teams);
     });
 }
 
+function initEvents() {
+  $("#teamsTable tbody").addEventListener("click", e => {
+    if (e.target.matches("a.remove-btn")) {
+      const id = e.target.dataset.id;
+      deleteTeamRequest(id).then(status => {
+        if (status.success) {
+          console.warn("delete done", status);
+          loadTeams();
+        }
+      });
+    }
+  });
+}
+
 loadTeams();
+initEvents();

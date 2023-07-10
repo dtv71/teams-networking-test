1b99067ce7fadf3b41e66979803d99026602dd6d
dtv71 <developer@codecloud.ro>
2023-07-10 17:53:55 +0300

    extract middleware functions

--- src/index.js
+++ src/index.js
@@ -1,55 +1,11 @@
+import { updateTeamRequest, createTeamRequest, deleteTeamRequest, loadTeamsRequest } from "./middleware";
+//import * as middleware from "./middleware";
 import "./style.css";
 import { $, filterElements, mask, unmask } from "./utilities";
 
 let allTeams = [];
 let editId;
 const form = "#teamsForm";
-
-function loadTeamsRequest() {
-  return fetch("http://localhost:3000/teams-json", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(r => r.json());
-}
-
-function deleteTeamRequest(id, callback) {
-  return fetch("http://localhost:3000/teams-json/delete", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ id })
-  })
-    .then(r => r.json())
-    .then(status => {
-      if (typeof callback === "function") {
-        callback(status);
-      }
-      return status;
-    });
-}
-
-function updateTeamRequest(team) {
-  return fetch("http://localhost:3000/teams-json/update", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(team)
-  }).then(r => r.json());
-}
-
-function createTeamRequest(team) {
-  return fetch("http://localhost:3000/teams-json/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(team)
-  }).then(r => r.json());
-}
 
 function getTeamAsHTML({ id, promotion, members, name, url }) {
   const displayUrl = url.startsWith("https://github.com/") ? url.substring(19) : url;
@@ -134,6 +90,7 @@ async function onSubmit(e) {
 
   if (editId) {
     team.id = editId;
+    //status = await middleware.updateTeamRequest(team);
     status = await updateTeamRequest(team);
     if (status.success) {
       allTeams = allTeams.map(t => {
